84387ace6aee22072105c7a55f680702ad772f9f
kotar0 <kotaro@temotoe.com>
2023-12-28 13:15:22 +0900

    fix bug

--- .tools/hello_world.sh
+++ .tools/hello_world.sh
@@ -9,14 +9,28 @@ xcode-select --install
 # Check for existing SSH key, generate if not present, and copy to clipboard.
 # SSH Key Generation and GitHub Setup
 ssh_key="$HOME/.ssh/id_rsa.pub"
+
+# .sshディレクトリが存在するか確認し、存在しない場合は作成
+if [ ! -d "$HOME/.ssh" ]; then
+	echo "Creating .ssh directory..."
+	mkdir -p "$HOME/.ssh"
+	chmod 700 "$HOME/.ssh"
+fi
+
 if [ ! -f "$ssh_key" ]; then
 	echo "Generating SSH keys..."
-	ssh-keygen -t rsa -b 4096 -C "kotaro@temotoe.com" # Replace with your email
+	ssh-keygen -t rsa -b 4096 -C "kotaro@temotoe.com" -f "$HOME/.ssh/id_rsa" # パスを明示的に指定
 	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_rsa
+	ssh-add "$HOME/.ssh/id_rsa"
 fi
-pbcopy <"$ssh_key"
-echo "SSH key copied to clipboard. Please add it to your GitHub account. Press enter to continue."
+
+# pbcopyが使用可能かどうかを確認し、SSHキーをクリップボードにコピー
+if command -v pbcopy &>/dev/null; then
+	pbcopy <"$ssh_key"
+	echo "SSH key copied to clipboard. Please add it to your GitHub account. Press enter to continue."
+else
+	echo "pbcopy is not available. Please manually copy the SSH key from $ssh_key"
+fi
 read -r
 
 # Pull config files from github
