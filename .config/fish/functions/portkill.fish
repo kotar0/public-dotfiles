ef926d98dacd5f05fdf005e030e21b392591fb45
kotar0 <kotaro@temotoe.com>
2024-01-17 03:05:58 +0900

    add fish function (portkill)

---
+++ .config/fish/functions/portkill.fish
@@ -0,0 +1,22 @@
+function portkill
+    if count $argv >/dev/null
+        set port $argv[1]
+        echo "Trying to kill process on port $port"
+
+        set pid_list (lsof -t -i :$port)
+
+        if count $pid_list >/dev/null
+            for pid in $pid_list
+                if kill $pid >/dev/null 2>&1
+                    echo "Successfully killed process with PID $pid on port $port"
+                else
+                    echo "Failed to kill process with PID $pid"
+                end
+            end
+        else
+            echo "No processes are running on port $port"
+        end
+    else
+        echo "Usage: portkill <port>"
+    end
+end
